0803b609f88af35695ecc00157f50ae6577cd81b
FRI <fri@marvin.(none)>
2013-11-17 21:08:10 -0600

    Basic code layout done

--- src/basic_state.py
+++ src/basic_state.py
@@ -6,11 +6,30 @@ import smach
 import smach_ros
 import geometry_msgs
 import move_base
+import move_base_msgs
 
-pub = rospy.publisher('move_base_simple/goal', PoseStamped)
 door_front = PoseStamped()
 door_back = PoseStamped()
 elevator_front = PoseStamped()
+client = SimpleActionClient()
+
+
+
+def move(loc, name):
+
+class Move(smach.State):
+    def __init__(self, loc, name):
+        smach.State.__init__(self, outcomes=['done'])
+
+    def execute(self, userdata):
+        goal = MoveBaseActionGoal()
+        goal.header.frame_id = "/map"
+        goal.goal = loc
+        goal.goal_id.stamp = ros.Time()
+        goal.goal_id.id = name
+        client.send_goal(goal)
+        client.wait_for_result(rospy.Duration.from_sec(1000.0))
+        return 'done'
 
 # define state CheckDoors
 class CheckDoors(smach.State):
@@ -30,7 +49,10 @@ class MoveToDoor(smach.State):
     def execute(self, userdata):
         #rospy.loginfo('Executing state MoveToDoor')
         # Move to the door here and detect if it's open
-	return 'door_open'
+        
+
+        move(door_front, "door_front")
+	return 'door_closed'
 
 # define state AskForHelpWithDoor
 class AskForHelpWithDoor(smach.State):
@@ -42,18 +64,8 @@ class AskForHelpWithDoor(smach.State):
         # Move to the door here and detect if it's open
         return 'door_open'
 
-
-# define state GoThroughDoor
-class GoThroughDoor(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['successfull','unsuccessfull'])
-
-    def execute(self, userdata):
-        #rospy.loginfo('Executing state GoThroughDoor')
-        return 'successfull'
-
 # define state GoToElevator
-class GoToElevator(smach.State):
+class MoveToElevator1(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['elevator_open'])
 
@@ -71,8 +83,68 @@ class GoToElevator(smach.State):
         elevator_front.pose.position.z = 0.0
         elevator_front.pose.position.w = 1.0
 
-        pub.publish(elevator_front)
+        move(elevator_front, "elevator_front")
+        return 'wait_elevator'
+
+class WaitForElevator1(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['elevator_open'])
+
+    def execute(self, userdata):
         return 'elevator_open'
+
+class NavigateElevatorDown(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['2nd_floor'])
+
+    def execute(self, userdata):
+
+        move(elevator_back, "elevator_back")
+
+        move(elevator_front, "elevator_front")
+        
+        return '2nd_floor'
+
+class MoveToCafe(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['at_cafe'])
+
+    def execute(self, userdata):
+        return 'at_cafe'
+
+class MoveToElevator2(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['at_elevator'])
+
+    def execute(self, userdata):
+        return 'at_elevator'
+
+class WaitForElevator2(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['elevator_open'])
+
+    def execute(self, userdata):
+        return 'elevator_open'
+
+class NavigateElevatorUp(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['3rd_floor'])
+        
+    def execute(self, userdata):
+        move(elevator_back, "elevator_back")
+
+        move(elevator_front, "elevator_front")
+
+        return '3rd_floor'
+
+class MoveToLab(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['at_lab'])
+
+    def execute(self, userdata):
+        move(lab, "lab")
+
+        return 'at_lab'
 
 def main():
     rospy.init_node('smach_example_state_machine')
@@ -83,16 +155,27 @@ def main():
     # Open the container
     with sm:
         # Add states to the container
-#        smach.StateMachine.add('CHECKDOORS', CheckDoors(), 
-#                               transitions={'more_doors':'MOVETODOOR', 'no_more_doors':'GOTOELEVATOR'})
-        # smach.StateMachine.add('MOVETODOOR', MoveToDoor(), 
-        #                        transitions={'door_open':'GOTHROUGHDOOR','door_closed':'ASKFORHELPWITHDOOR'})
-	# smach.StateMachine.add('ASKFORHELPWITHDOOR', AskForHelpWithDoor(),
-        #                        transitions={'door_open':'GOTHROUGHDOOR'})
-	# smach.StateMachine.add('GOTHROUGHDOOR', GoThroughDoor(),
-        #                        transitions={'successfull':'GOTOELEVATOR','unsuccessfull':'ASKFORHELPWITHDOOR'})
-	smach.StateMachine.add('GOTOELEVATOR', GoToElevator(),
-                               transitions={'elevator_open':'elevator_open'})
+	smach.StateMachine.add('GOTOELEVATOR1', Move(elevator3_front, "elevator3_front"),
+                               transitions={'done':'WAITFORELEVATOR1'})
+        smach.StateMachine.add('WAITFORELEVATOR1', Wait(),
+                               transitions={'done':'NAVELEVATORDOWN'})
+        smach.StateMachine.add('NAVELEVATORDOWN', NavigateElevator(elevator3_back, elevator3_front),
+                               transitions={'done':'SWITCHTO2'})
+        smach.StateMachine.add('SWITCHTO2', Wait(),
+                               transitions={'done':'MOVETOCAFE'})
+        smach.StateMachine.add('MOVETOCAFE', Move(cafe, 'cafe'),
+                               transitions={'done':'MOVETOELEVATOR2'})
+        smach.StateMachine.add('MOVETOELEVATOR2', Move(elevator2_front, 'elevator2_front'),
+                               transitions={'done':'WAITFORELEVATOR2'})
+        smach.StateMachine.add('WAITFORELEVATOR2', Wait(),
+                               transitions={'done','NAVIGATEELEVATORUP'})
+        smach.StateMachine.add('NAVELEVATORUP', NavigateElevator(elevator2_back, elevator2_front),
+                               transitions={'done':'SWITCHTO3'})
+        smach.StateMachine.add('SWITCHTO3', Wait(),
+                               transitions={'done':'MOVETOLAB'})
+        smach.StateMachine.add('MOVETOLAB', Move(lab, "lab"),
+                               transitions={'done':'at_lab'})
+                               
 
     # Create and start the introspection server
     sis = smach_ros.IntrospectionServer('coffee_state_server', sm, '/SM_ROOT')
